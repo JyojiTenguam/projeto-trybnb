817db56186ba1080a8b3033491ad4580eb7f4881
JyojiTenguam <j.y.oji@hotmail.com>
2024-12-03 16:49:04 -0300

    'Resolvendo requisito 03 do projeto Trybnb'

--- app/src/main/java/com/betrybe/trybnb/ui/views/activities/MainActivity.kt
+++ app/src/main/java/com/betrybe/trybnb/ui/views/activities/MainActivity.kt
@@ -2,12 +2,45 @@ package com.betrybe.trybnb.ui.views.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.betrybe.trybnb.R
+import com.betrybe.trybnb.ui.views.fragments.CreateReservationFragment
+import com.betrybe.trybnb.ui.views.fragments.ProfileFragment
+import com.betrybe.trybnb.ui.views.fragments.ReservationFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.navigation_bottom_view)
+
+        replaceFragment(ReservationFragment())
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.reservation_menu_item -> {
+                    replaceFragment(ReservationFragment())
+                    true
+                }
+                R.id.create_reservation_menu_item -> {
+                    replaceFragment(CreateReservationFragment())
+                    true
+                }
+                R.id.profile_menu_item -> {
+                    replaceFragment(ProfileFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_fragment_container, fragment)
+            .commit()
     }
 }
