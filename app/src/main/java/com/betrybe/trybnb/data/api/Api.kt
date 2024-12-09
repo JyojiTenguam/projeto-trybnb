336871f8b577b6c2bbaf6ed076eb3c4fcdbe5ce6
JyojiTenguam <j.y.oji@hotmail.com>
2024-12-09 15:51:57 -0300

    'Resolvendo requisito 08 do projeto Trybnb'

--- app/src/main/java/com/betrybe/trybnb/data/api/Api.kt
+++ app/src/main/java/com/betrybe/trybnb/data/api/Api.kt
@@ -1,12 +1,26 @@
 package com.betrybe.trybnb.data.api
 
+import com.betrybe.trybnb.data.models.BookingIdItem
 import com.betrybe.trybnb.data.models.LoginRequest
 import com.betrybe.trybnb.data.models.LoginResponse
+import com.betrybe.trybnb.data.models.ReservationItem
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface Api {
     @POST("auth")
     suspend fun doLoginRequest(@Body loginRequest: LoginRequest): Response<LoginResponse>
+
+    @GET("booking?checkin=2024-01-01&checkout=2024-12-21")
+    suspend fun getAllBookings(): List<BookingIdItem>
+
+    @GET("booking/{Id}")
+    suspend fun getBookingById(
+        @Path("Id") bookingId: String,
+        @Header("Accept") accept: String = "application/json"
+    ): ReservationItem
 }
